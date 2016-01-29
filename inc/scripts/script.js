6e1f6e5c6cce0cb50b21cc19efa74aca4e5cdd85
Justin He <justinheeftw@gmail.com>
2016-01-29 11:19:09 -0500

    added background

---
+++ inc/scripts/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('#banner .heading').fadeIn( 600, function() {
+  });
+  $('#banner .secondheading').fadeIn( 1000, function() {
+  });
+  $('#banner .thirdheading').fadeIn( 1400, function() {
+  });
+  $('#banner #links').fadeIn( 1400, function(){}).css("display","block");
+  $('#prof').fadeIn( 1400, function(){}).css("display","block");
+  particlesJS.load('particles-js', 'inc/scripts/particles.json', function() {
+  console.log('particles.js loaded - callback');
+  });
+});
