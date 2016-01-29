7e6dff2386a7b3472a0d246e551d50aadc226ee1
Justin He <justinheeftw@gmail.com>
2016-01-29 12:01:54 -0500

    fixed jquery assisted opening

--- inc/scripts/script.js
+++ inc/scripts/script.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
-  $('#banner .heading').fadeIn( 600, function() {
-  });
-  $('#banner .secondheading').fadeIn( 1000, function() {
-  });
-  $('#banner .thirdheading').fadeIn( 1400, function() {
-  });
+  $('#banner .heading').fadeIn( 600, function(){});
+  $('#banner .secondheading').fadeIn( 1000, function(){});
+  $('#banner .thirdheading').fadeIn( 1400, function(){});
   $('#banner #links').fadeIn( 1400, function(){}).css("display","block");
   $('#prof').fadeIn( 1400, function(){}).css("display","block");
+  $('#scroll').fadeIn({queue: false, duration: 1400}, function(){}).css("display","block");
+  $('#scroll').animate({ top: "4%" }, 1400, function(){});
 });
