d7a2aeca439f03baddd0d2fcc0f2d6f6674f6bab
Justin He <justinheeftw@gmail.com>
2016-02-22 13:48:22 -0500

    will particles.js work

--- inc/scripts/script.js
+++ inc/scripts/script.js
@@ -1,3 +1,8 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'inc/scripts/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
 $(document).ready(function(){
   $('#banner .heading').fadeIn( 600, function(){});
   $('#banner .secondheading').fadeIn( 1000, function(){});
@@ -6,4 +11,22 @@ $(document).ready(function(){
   $('#prof').fadeIn( 1400, function(){}).css("display","block");
   $('#scroll').fadeIn({queue: false, duration: 1400}, function(){}).css("display","block");
   $('#scroll').animate({ top: "4%" }, 1400, function(){});
+
+
+  $(window).scroll( function(){
+        $('.fadeInScroll').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
+            bottom_of_window = bottom_of_window - 500;  
+          
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'}, 1400);
+            }
+        }); 
+    });
+
 });
