9b0fcfb456ba94c7c3a28b974dbfc33263691c68
Justin He <justinheeftw@gmail.com>
2016-03-08 15:15:25 -0500

    fadeinfooter

--- inc/scripts/script.js
+++ inc/scripts/script.js
@@ -28,6 +28,21 @@ $(document).ready(function(){
             }
         }); 
 
+        $('.fadeInScrollFooter').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
+            bottom_of_window = bottom_of_window + 100;  
+          
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'}, 1400);
+            }
+        }); 
+
+
         $('.fadeInScrollInterest').each( function(i){
             
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
